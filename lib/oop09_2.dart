7c2c4f978f374702778f2b7330350a9740910d54
arina-cdemy <arina.misakyan.cdemy@gmail.com>
2025-10-06 17:07:04 +0200

    refactor: update analysis options and improve documentation in Student class

--- lib/oop09_2.dart
+++ lib/oop09_2.dart
@@ -1,13 +1,16 @@
+///defining class [Student]
 class Student {
+  ///object for getting the first name as a string
   final String? firstName;
+  ///object for getting the age
   final int? age;
 
-
+///the constructor [Student] that allows to initialize the objects
   Student({
     required this.firstName,
     required this.age,
   });
-
+///using the copyWith() method to make a copy of the class and change the attributes 
   Student copyWith({
     String? firstName,
     int? Function()? age
